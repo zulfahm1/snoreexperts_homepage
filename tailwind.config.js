7087429225c8d2291a7733ffc82023af5802e0b3
iyasmzn <iyasmzn07@gmail.com>
2021-06-16 21:25:46 +0700

    update tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
+  purge: {
+    enabled: true,
+    content: [
+        './**/*.html'
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     container: {
